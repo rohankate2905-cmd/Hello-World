98848946d7dee01be8001fa2fe097197ff6067b6
rohankate2905-cmd <rohankate2905@gmail.com>
2026-07-09 19:20:13 +0530

    Add files via upload

---
+++ pattern printing .c
@@ -0,0 +1,20 @@
+//NAME:KATE ROHAN VISHNU 
+//PRN:2503033111372040
+//TITLE:PATTERN PRINTING 3
+
+
+
+#include<stdio.h>
+int main(){
+    int i,j,k;
+    for(i=1;i<=3;i++){
+        for(k=1;k<i;k++){
+            printf("\t");
+        }
+        for(j=i;j<=3;j++){
+            printf("*\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
