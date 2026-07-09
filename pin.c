5f26be9d97bd1f0a18c0ab26b5684326ac0262a3
rohankate2905-cmd <rohankate2905@gmail.com>
2026-07-09 19:21:57 +0530

    Add files via upload

---
+++ pin.c
@@ -0,0 +1,17 @@
+//NAME: ROHAN vishnu kate
+//prn 2503033111372040
+
+#include <stdio.h>
+ int main(){
+ 	int spin , upin;
+ 	spin=1845;
+ 	printf("Enter the pin:\t");
+ 	scanf("%d", &upin);
+ 	if(spin==upin){
+ 		printf("Correct pin");
+	 }
+	 else{
+	 	printf("Incorrect pin");
+	 }
+	 return 0;
+ }
