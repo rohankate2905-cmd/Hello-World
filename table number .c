4c1d871ad0f2e2ef38b78c6f6a9f594179d29432
rohankate2905-cmd <rohankate2905@gmail.com>
2026-07-09 19:20:38 +0530

    Add files via upload

---
+++ table number .c
@@ -0,0 +1,19 @@
+NAME KATE ROHAN VISHNU 
+//PRN:2503033111372040
+//TITLE:TABLE OF NUMBER
+
+
+
+#include<stdio.h>
+int main(){
+    int n, i;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= 10; i++){
+        printf("%d * %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
