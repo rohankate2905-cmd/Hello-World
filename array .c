9daaeceb2e054c7f32d4842d2b9e9e9bb517988d
rohankate2905-cmd <rohankate2905@gmail.com>
2026-07-09 19:17:32 +0530

    Add files via upload

---
+++ array .c
@@ -0,0 +1,25 @@
+// NAME:KATE ROHAN VISHNU 
+//PRN:25030331111372040
+//TITLE:ADDITION OF TWO ARRAY
+
+
+
+
+#include<stdio.h>
+int main(){
+    int a[5]={11,12,13,14,15};
+    int b[5]={91,81,71,61,15};
+    int c[5];
+    int i;
+    
+    for(i=0; i<=4; i++){
+        c[i]=a[i]+b[i];
+    }
+    
+    printf("Addition of two array is : ");
+    for(i=0;i<=4;i++){
+        printf(" %d\t",c[i]);
+    }
+    
+    return 0;
+}
